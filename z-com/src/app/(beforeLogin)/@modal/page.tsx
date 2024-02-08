31f4e05084ad178cb87d0aa6f4abd270a09c5a2c
류동하 <06dongha@gmail.com>
2024-02-08 21:14:16 +0900

    asd

---
+++ z-com/src/app/(beforeLogin)/@modal/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Page() {
+  const [id] = useState();
+  const [password] = useState();
+  const [message] = useState();
+
+  const onSubmit = () => {};
+
+  const onClickClose = () => {};
+
+  const onChangeId = () => {};
+
+  const onChangePassword = () => {};
+
+  return(
+    
+  )
+}
--- z-com/src/app/(beforeLogin)/i/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "../../../../../node_modules/next/navigation";
-
-export default function Login() {
-  redirect("i/flow/login");
-}
